8effe9af6e8cf6045522329ec8b4c1fcbae3e9cc
Lilia Lomaka <90831568+llomaka@users.noreply.github.com>
2022-07-08 10:45:33 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,28 +2,33 @@ import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import contactsApi from '../service/contactsApi';
-import authorizationApi from '../service/connectionsApi';
-import filterSlice from './filterSlice';
+import { authReducer } from './authorization';
+import filterReducer from './filterSlice';
 
-const persistConfig = {
+const filterPersistConfig = {
   key: 'filter',
   storage,
 }
-
-const persistedReducer = persistReducer(persistConfig, filterSlice);
+const authorizationPersistConfig = {
+  key: 'authorization',
+  storage,
+  whitelist: ['token'],
+}
+const filterPersistedReducer = persistReducer(filterPersistConfig, filterReducer);
+const authorizationPersistedReducer = persistReducer(authorizationPersistConfig, authReducer);
 
 export const store = configureStore({
   reducer: {
     [contactsApi.reducerPath]: contactsApi.reducer,
-    [authorizationApi.reducerPath]: authorizationApi.reducer,
-    filter: persistedReducer,
+    authorization: authorizationPersistedReducer,
+    filter: filterPersistedReducer,
   },
    middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
-    }).concat(contactsApi.middleware).concat(authorizationApi.middleware),
+    }).concat(contactsApi.middleware),
 });
 
 export const persistor = persistStore(store);
