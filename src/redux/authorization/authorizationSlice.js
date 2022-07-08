dc3f430e2cacb7136516cf2510c008afb93b74c8
Lilia Lomaka <90831568+llomaka@users.noreply.github.com>
2022-07-08 10:44:05 +0300

    Create authorizationSlice.js

---
+++ src/redux/authorization/authorizationSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from '@reduxjs/toolkit';
+import authorizationOperations from './authorizationOperations';
+
+const initialState = {
+  user: { name: null, email: null },
+  token: null,
+  isLoggedIn: false,
+};
+
+const authorizationSlice = createSlice ({
+  name: 'authorization',
+  initialState,
+  extraReducers: {
+    [authorizationOperations.createUser.fulfilled](state, action) {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.isLoggedIn = true;
+    },
+    [authorizationOperations.loginUser.fulfilled](state, action) {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.isLoggedIn = true;
+    },
+    [authorizationOperations.logoutUser.fulfilled](state, action) {
+      state.user = { name: null, email: null };
+      state.token = null;
+      state.isLoggedIn = false;
+    },
+    [authorizationOperations.getCurrentUserInfo.fulfilled](state, action) {
+      state.user = action.payload;
+      state.isLoggedIn = true;
+    },
+  },
+});
+
+export default authorizationSlice.reducer;
