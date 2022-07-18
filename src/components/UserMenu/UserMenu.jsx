5814eb4197fab8254682f83367873305a58fb9f1
Lilia Lomaka <llomaka80@gmail.com>
2022-07-18 18:53:25 +0300

    Обробка реєстрації-входу-виходу користувача

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,22 +1,15 @@
-// import { useGetCurrentUserInfoQuery, useLogoutUserMutation, useLoginUserMutation } from 'redux/authorizationSlice';
+import { authSelectors, authOperations } from 'redux/authorization';
+import { useSelector, useDispatch } from 'react-redux';
 import styles from './UserMenu.module.css';
 
 export default function UserMenu() {
-  // const [logoutUser, { data: info, error, isUninitialized, isLoading, isSuccess, isError }] = useLogoutUserMutation();
-  // console.log('data:', info, ';', 'error:', error, ';', 'isUninitialized:', isUninitialized, ';', 'isLoading', isLoading, ';', 'isSuccess:', isSuccess, ';', 'isError:', isError);
-  // const [loginUser, { data }] = useLoginUserMutation();
-  // const {data: currentUser} = useGetCurrentUserInfoQuery(token);
-  // console.log(data);
-
-  function handleLogoutClick(token) {
-    if (!token) return;
-    // logoutUser(token);
-  }
+  const username = useSelector(authSelectors.getUsername);
+  const dispatch = useDispatch();
 
   return (
     <div className={styles.menu}>
-      {/* <p>Welcome, {data.user.name}</p> */}
-      <p className={styles.button} onClick={() => handleLogoutClick()}>Logout</p>
+      <p>Welcome, {username}</p>
+      <p className={styles.button} onClick={() => dispatch(authOperations.logoutUser())}>Logout</p>
     </div>
   );
 }
