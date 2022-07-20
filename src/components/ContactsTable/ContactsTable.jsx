788bb05ee4444c76d2a948d0319dfdcaa08c2ae4
Lilia Lomaka <llomaka80@gmail.com>
2022-07-20 09:18:03 +0300

    Початок переробки переліку контактів на таблицю

--- src/components/ContactsTable/ContactsTable.jsx
+++ src/components/ContactsTable/ContactsTable.jsx
@@ -1,29 +1,29 @@
 import ContactForm from './ContactForm';
-import ContactList from './ContactList';
-import { useSelector, useDispatch } from 'react-redux';
-import { useMemo } from 'react';
-import contactsSelector from 'redux/contactsSelectors';
-import filterSelector from 'redux/filterSelectors';
-import { getAllContacts } from 'redux/contactsOperations';
+// import ContactList from './ContactList';
+// import { useSelector } from 'react-redux';
+// import { useMemo } from 'react';
+// import contactsSelector from 'redux/contactsSelectors';
+// import filterSelector from 'redux/filterSelectors';
 import styles from './ContactsTable.module.css';
 
 export default function ContactsTable() {
-  const { data = [], isError, error, isLoading, isSuccess } = useGetAllContactsQuery();
-  const filter = useSelector(state => state.filter.value);
-  const renderContactList = useMemo(() =>
-    data
-      .filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()))
-      .sort((a, b) => a.name.localeCompare(b.name)
-    ), [data, filter]);
+  // const filter = useSelector(filterSelector);
+  // const contacts = useSelector(contactsSelector);
+  // const renderContactList = useMemo(() =>
+  //   contacts
+  //     .filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()))
+  //     .sort((a, b) => a.name.localeCompare(b.name)
+  //   ), [contacts, filter]);
 
   return (
     <div>
       <h1 className={styles.header}>Phonebook</h1>
       <ContactForm />
       <h2 className={styles.subheader}>Contacts</h2>
-      {isError && (<p>An error occurred: {error}</p>)}
+      {/* {isError && (<p>An error occurred: {error}</p>)}
       {isLoading && <p>Loading...</p>}
-      {isSuccess && <ContactList contacts={renderContactList} />}
+      {isSuccess && <ContactList contacts={renderContactList} />} */}
+
     </div>
   );
 };
