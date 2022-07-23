cbf4cd80079c1bc1772e9300eff753639b8f4f60
Lilia Lomaka <llomaka80@gmail.com>
2022-07-24 00:37:07 +0300

    Форматування тексту

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -15,7 +15,7 @@ export default function SharedLayout() {
   return (
     <>
       <Header>
-        <Container maxWidth='lg' sx={{display: 'flex', gap: '5px', justifyContent: 'space-between'}}>
+        <Container maxWidth='lg' sx={{ display: 'flex', gap: '5px', justifyContent: 'space-between' }}>
           <AppNameBar />
           {!isUserLoggedIn && <AuthNavigationBar />}
           {isUserLoggedIn && <UserMenu />}
