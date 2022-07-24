f65a8ef211506e6800e1962343fa4ef7c8bae03e
Lilia Lomaka <llomaka80@gmail.com>
2022-07-24 14:35:19 +0300

    Зміна форм входу та реєстрації відповідно до зауважень ментора

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,9 +1,7 @@
 import { useState, useId } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
 import { authOperations, authSelectors } from 'redux/authorization';
-import useSnackbar from 'hooks/useSnackbar';
-import { Snackbar, Avatar, TextField, Link, Grid, Box, Typography, Container } from '@mui/material';
+import { Avatar, TextField, Link, Grid, Box, Typography, Container } from '@mui/material';
 import LoadingButton from '@mui/lab/LoadingButton';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
@@ -12,8 +10,6 @@ export default function LoginForm() {
   const [password, setPassword] = useState('');
   const id = useId();
   const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const { open, message, setMessage, handleClose } = useSnackbar();
   const isSigningInUser = useSelector(authSelectors.isSigningInUser);
 
   const handleInputChange = event => {
@@ -25,88 +21,77 @@ export default function LoginForm() {
     }
   };
 
-  const resetForm = () => {
-    setEmail('');
-    setPassword('');
-  };
-
   const handleSubmit = async (event) => {
     event.preventDefault();
     dispatch(authOperations.loginUser({ email, password }));
-    setMessage(`User with ${email} is successfully logged in!`);
-    resetForm();
-    navigate('/contacts');
   };
 
   return (
-    <>
-      <Container component='div' maxWidth='xs'>
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component='h2' variant='h5'>
-            Sign in
-          </Typography>
-          <Box component='form' onSubmit={handleSubmit} sx={{ mt: 1 }}>
-            <TextField
-              margin='normal'
-              required
-              fullWidth
-              label='Email Address'
-              autoComplete='email'
-              autoFocus
-              type='email'
-              name='email'
-              title='Email address'
-              id={id + 'email'}
-              placeholder='olomaka@gmail.com'
-              onChange={handleInputChange}
-              value={email}
-            />
-            <TextField
-              margin='normal'
-              required
-              fullWidth
-              label='Password'
-              autoComplete='current-password'
-              type='password'
-              name='password'
-              minLength={8}
-              title='Password must be longer, than 8 characters, contain at least one number and one uppercase character, not contain spaces and parentheses'
-              id={id + 'password'}
-              placeholder='pa$sw0rD'
-              onChange={handleInputChange}
-              value={password}
-            />
-            <LoadingButton
-              onClick={handleSubmit}
-              type='submit'
-              fullWidth
-              variant='contained'
-              sx={{ mt: 3, mb: 2 }}
-              loading={isSigningInUser}
-            >
-              Sign In
-            </LoadingButton>
-            <Grid container justifyContent='flex-end'>
-              <Grid item>
-                <Link href='/goit-react-hw-08-phonebook/register' variant='body2'>
-                  Don't have an account? Sign Up
-                </Link>
-              </Grid>
+    <Container component='div' maxWidth='xs'>
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component='h2' variant='h5'>
+          Sign in
+        </Typography>
+        <Box component='form' onSubmit={handleSubmit} sx={{ mt: 1 }}>
+          <TextField
+            margin='normal'
+            required
+            fullWidth
+            label='Email Address'
+            autoComplete='email'
+            autoFocus
+            type='email'
+            name='email'
+            title='Email address'
+            id={id + 'email'}
+            placeholder='olomaka@gmail.com'
+            onChange={handleInputChange}
+            value={email}
+          />
+          <TextField
+            margin='normal'
+            required
+            fullWidth
+            label='Password'
+            autoComplete='current-password'
+            type='password'
+            name='password'
+            minLength={8}
+            title='Password must be longer, than 8 characters, contain at least one number and one uppercase character, not contain spaces and parentheses'
+            id={id + 'password'}
+            placeholder='pa$sw0rD'
+            onChange={handleInputChange}
+            value={password}
+          />
+          <LoadingButton
+            onClick={handleSubmit}
+            type='submit'
+            fullWidth
+            variant='contained'
+            sx={{ mt: 3, mb: 2 }}
+            loading={isSigningInUser}
+          >
+            Sign In
+          </LoadingButton>
+          <Grid container justifyContent='flex-end'>
+            <Grid item>
+              <Link href='/goit-react-hw-08-phonebook/register' variant='body2'>
+                Don't have an account? Sign Up
+              </Link>
             </Grid>
-          </Box>
+          </Grid>
         </Box>
-      </Container>
-      <Snackbar autoHideDuration={1000} open={open} onClose={handleClose} message={message} />
-    </>
+      </Box>
+    </Container>
   );
 }
--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,11 +1,9 @@
 import { useState, useId } from 'react';
 import { authOperations, authSelectors } from 'redux/authorization';
-import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { Avatar, TextField, Link, Grid, Box, Typography, Container, Snackbar } from '@mui/material';
+import { Avatar, TextField, Link, Grid, Box, Typography, Container } from '@mui/material';
 import LoadingButton from '@mui/lab/LoadingButton';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import useSnackbar from 'hooks/useSnackbar';
 
 export default function RegisterForm() {
   const [name, setName] = useState('');
@@ -13,8 +11,6 @@ export default function RegisterForm() {
   const [password, setPassword] = useState('');
   const id = useId();
   const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const { open, message, setMessage, handleClose } = useSnackbar();
   const isCreatingUser = useSelector(authSelectors.isCreatingUser);
 
   const handleInputChange = event => {
@@ -28,109 +24,97 @@ export default function RegisterForm() {
     }
   };
 
-  const resetForm = () => {
-    setName('');
-    setEmail('');
-    setPassword('');
-  };
-
   const handleSubmit = async (event) => {
     event.preventDefault();
     dispatch(authOperations.createUser({ name, email, password }));
-    setMessage(`User ${name} is successfully created!`);
-    resetForm();
-    navigate('/contacts');
   };
 
   return (
-    <>
-      <Container component='div' maxWidth='xs'>
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component='h2' variant='h5'>
-            Sign up
-          </Typography>
-          <Box component='form' noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  autoComplete='name'
-                  name='name'
-                  required
-                  fullWidth
-                  id={id + 'name'}
-                  label='Full Name'
-                  autoFocus
-                  type='text'
-                  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                  placeholder='Oleksandra Lomaka'
-                  onChange={handleInputChange}
-                  value={name}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  label='Email Address'
-                  autoComplete='email'
-                  type='email'
-                  name='email'
-                  title='Email address'
-                  id={id + 'email'}
-                  placeholder='olomaka@gmail.com'
-                  onChange={handleInputChange}
-                  value={email}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  label='Password'
-                  autoComplete='new-password'
-                  type='password'
-                  name='password'
-                  minLength={8}
-                  title='Password must be longer, than 8 characters, preferably contain at least one number and one uppercase character, not contain spaces and parentheses'
-                  id={id + 'password'}
-                  placeholder='pa$sw0rD'
-                  onChange={handleInputChange}
-                  value={password}
-                />
-              </Grid>
+    <Container component='div' maxWidth='xs'>
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component='h2' variant='h5'>
+          Sign up
+        </Typography>
+        <Box component='form' noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                autoComplete='name'
+                name='name'
+                required
+                fullWidth
+                id={id + 'name'}
+                label='Full Name'
+                autoFocus
+                type='text'
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                placeholder='Oleksandra Lomaka'
+                onChange={handleInputChange}
+                value={name}
+              />
             </Grid>
-            <LoadingButton
-              type='submit'
-              fullWidth
-              variant='contained'
-              sx={{ mt: 3, mb: 2 }}
-              onClick={handleSubmit}
-              loading={isCreatingUser}
-            >
-              Sign Up
-            </LoadingButton>
-            <Grid container justifyContent='flex-end'>
-              <Grid item>
-                <Link href='/goit-react-hw-08-phonebook/login' variant='body2'>
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                label='Email Address'
+                autoComplete='email'
+                type='email'
+                name='email'
+                title='Email address'
+                id={id + 'email'}
+                placeholder='olomaka@gmail.com'
+                onChange={handleInputChange}
+                value={email}
+              />
             </Grid>
-          </Box>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                label='Password'
+                autoComplete='new-password'
+                type='password'
+                name='password'
+                minLength={8}
+                title='Password must be longer, than 8 characters, preferably contain at least one number and one uppercase character, not contain spaces and parentheses'
+                id={id + 'password'}
+                placeholder='pa$sw0rD'
+                onChange={handleInputChange}
+                value={password}
+              />
+            </Grid>
+          </Grid>
+          <LoadingButton
+            type='submit'
+            fullWidth
+            variant='contained'
+            sx={{ mt: 3, mb: 2 }}
+            onClick={handleSubmit}
+            loading={isCreatingUser}
+          >
+            Sign Up
+          </LoadingButton>
+          <Grid container justifyContent='flex-end'>
+            <Grid item>
+              <Link href='/goit-react-hw-08-phonebook/login' variant='body2'>
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
         </Box>
-      </Container>
-      <Snackbar autoHideDuration={1000} open={open} onClose={handleClose} message={message} />
-    </>
+      </Box>
+    </Container>
   );
-};
+}
