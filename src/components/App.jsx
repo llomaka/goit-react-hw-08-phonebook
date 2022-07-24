c58abd9c7c243c9c86fa2279db953b62e988669d
Lilia Lomaka <llomaka80@gmail.com>
2022-07-24 19:54:30 +0300

    Використання теми відповідно до браузера та ОС

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
-import { lazy, useEffect } from 'react';
+import { lazy, useEffect, useMemo } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { authOperations, authSelectors } from 'redux/authorization';
 import SharedLayout from './SharedLayout';
-import { Box, CssBaseline } from '@mui/material';
+import { Box, CssBaseline, useMediaQuery } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const LoginPage = lazy(() => import('../pages/LoginPage'));
 const RegisterPage = lazy(() => import('../pages/RegisterPage'));
@@ -13,6 +14,14 @@ export default function App() {
   const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
   const isFetchingCurrentUser = useSelector(authSelectors.getIsFetchingCurrentUser);
   const dispatch = useDispatch();
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = useMemo(() =>
+    createTheme({
+      palette: {
+        mode: prefersDarkMode ? 'dark' : 'light',
+      },
+  }), [prefersDarkMode]);
 
   useEffect(() => {
     dispatch(authOperations.getCurrentUserInfo());
@@ -23,17 +32,19 @@ export default function App() {
       height: '100vh',
     }}
   >
-    <CssBaseline />
-    <Routes>
-      <Route path="/*" element={<SharedLayout />}>
-        {isLoggedIn && <Route path='contacts' element={<ContactsPage />} />}
-        {!isLoggedIn && <>
-          <Route path='login' element={<LoginPage />} />
-          <Route path='register' element={<RegisterPage />} />
-        </>}
-        <Route path='*' element={<Navigate to={isLoggedIn ? '/contacts' : '/login'} replace />} />
-      </Route>
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Routes>
+        <Route path="/*" element={<SharedLayout />}>
+          {isLoggedIn && <Route path='contacts' element={<ContactsPage />} />}
+          {!isLoggedIn && <>
+            <Route path='login' element={<LoginPage />} />
+            <Route path='register' element={<RegisterPage />} />
+          </>}
+          <Route path='*' element={<Navigate to={isLoggedIn ? '/contacts' : '/login'} replace />} />
+        </Route>
+      </Routes>
+    </ThemeProvider>
   </Box>
   );
 }
